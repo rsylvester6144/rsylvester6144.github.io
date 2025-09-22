e30ebfa69d2dccbab77b4dd4fec983d14e77dc03
rsylvester6144 <rsylvester6144@gmail.com>
2025-09-21 23:02:48 -0400

    Initial commit.

---
+++ script.js
@@ -0,0 +1,76 @@
+function showSection(id) {
+    const sections = document.querySelectorAll("section");
+    sections.forEach(section => {
+        section.classList.remove("active");
+        section.classList.remove("fade-in");
+        section.classList.remove("slide-in");
+    });
+
+    const activeSection = document.getElementById(id);
+    activeSection.classList.add("active");
+    activeSection.classList.add("fade-in");
+    activeSection.classList.add("slide-in");
+}
+
+setTimeout(function() {
+    // Start the typewriter effect after 2 seconds
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+
+    // Inject CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+}, 2000); // Wait for 2 seconds (2000 milliseconds)
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    // This is now delayed by 2 seconds, so it will not run immediately
+};
